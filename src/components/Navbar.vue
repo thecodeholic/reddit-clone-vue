caeb9a6235551513e503c08b6025ed2d068ed551
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-08-01 09:41:35 +0400

    Install bootstrap-vue and create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="#">
+        reddit
+      </b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-input-group class="navbar-search px-5" size="sm">
+          <b-input-group-prepend is-text>
+            <b-icon icon="search"></b-icon>
+          </b-input-group-prepend>
+          <b-form-input placeholder="Search"></b-form-input>
+        </b-input-group>
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-form>
+            <b-button size="sm" variant="outline-primary mr-3">LOG IN</b-button>
+            <b-button size="sm" variant="primary mr-3">SIGN UP</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              <b-icon icon="person-fill"></b-icon>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style lang="scss" scoped>
+  .navbar-search {
+    width: auto;
+    flex: 1;
+  }
+
+  button {
+    width: 100px;
+  }
+</style>
---
+++ src/plugins/bootstrap.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+import '../scss/index.scss';
