b6a07f2bb37c90c118fe6b5d38b4f4402e52f55d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-09-03 09:02:58 +0400

    Install vue-moment

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './plugins/bootstrap';
 import './plugins/font-awesome';
+import './plugins/moment';
 import store from './vuex/index';
 // import '@fortawesome/fontawesome-free/css/all.css'
 
